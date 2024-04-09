6935d106fd99b73d04ef7117d676320c28c9df50
Johns, Erik Michael <erikj3520@gmail.com>
2024-04-09 00:49:58 -0600

    Added Backend

--- src/Pages/Javascript/Contact.js
+++ src/Pages/Javascript/Contact.js
@@ -6,6 +6,60 @@ import Footer from "../../Components/Footer";
 
 const Contact = () => {
 
+    /* Email Server */
+
+    const initialFormData = {
+        firstName: '',
+        lastName: '',
+        email: '',
+        phone: '',
+        state: '',
+        preferredDate: '',
+        secondaryDate: '',
+        interest: '',
+        budget: '',
+        guests: '',
+        hear: '',
+        details: ''
+    }
+
+    const [formData, setFormData] = useState(initialFormData);
+
+    const handleChange = (event) => {
+        const {name, value} = event.target;
+        setFormData(prevState => ({
+            ...prevState,
+            [name]: value,
+        }));
+    };
+
+    const handleSubmit = async (event) => {
+        try {
+            event.preventDefault();
+
+            const response = await fetch('http://localhost:3001/send-email', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    formData: formData
+                }),
+            });
+
+            if (response.ok) {
+                console.log("Email sent successfully");
+                setFormData(initialFormData);
+            } else {
+                console.error("Email did not send.");
+            }
+        } catch (error) {
+            console.error("Error submitting data: ", error);
+        }
+    }
+
+    /* Phone Number Styling */
+
     const [phoneNumber, setPhoneNumber] = useState('');
 
     const formatPhoneNumber = (input) => {
@@ -14,10 +68,14 @@ const Contact = () => {
         return input.replace(/(\d{3})(\d{3})(\d{4})/, '($1) $2-$3');
     };
 
-    const handleChange = (event) => {
+    const handlePhoneChange = (event) => {
         const inputPhoneNumber = event.target.value;
         const formattedNumber = formatPhoneNumber(inputPhoneNumber);
         setPhoneNumber(formattedNumber);
+        setFormData(prevState => ({
+            ...prevState,
+            [phoneNumber]: formattedNumber
+        }));
     };
 
     return (
@@ -37,26 +95,26 @@ const Contact = () => {
                     </p>
                 </div>
 
-                <form>
+                <form onSubmit={handleSubmit}>
                     <div className={"contact-form-two"}>
                         <div className={"field-holder"}>
-                            <input id={"firstname"} type="text" required/>
+                            <input name={"firstName"} onChange={handleChange} value={formData.firstName} id={"firstname"} type="text" required/>
                             <label htmlFor={"firstname"}>First Name</label>
                         </div>
 
                         <div className={"field-holder"}>
-                            <input id={"lastname"} type="text" required/>
+                            <input name={"lastName"} onChange={handleChange} value={formData.lastName} id={"lastname"} type="text" required/>
                             <label htmlFor={"lastname"}>Last Name</label>
                         </div>
                     </div>
 
                     <div className={"field-holder"}>
-                        <input id={"email"} type="email" required/>
+                        <input name={"email"} onChange={handleChange} value={formData.email} id={"email"} type="email" required/>
                         <label htmlFor={"email"}>Email</label>
                     </div>
 
                     <div className={"field-holder"}>
-                        <input id={"phonenumber"} type="text" value={phoneNumber} onChange={handleChange} required/>
+                        <input name={"phone"} id={"phonenumber"} type="text" value={phoneNumber} onChange={handlePhoneChange} required/>
                         <label htmlFor={"phonenumber"}>Phone Number</label>
                     </div>
 
@@ -64,18 +122,18 @@ const Contact = () => {
 
                     <div className={"contact-form-two"}>
                         <div className={"field-holder"}>
-                            <input className="" type="date" required/>
+                            <input name={"preferredDate"} onChange={handleChange} value={formData.preferredDate} id={"preferredDate"} type="date" required/>
                             <label>Preferred Date</label>
                         </div>
 
                         <div className={"field-holder"}>
-                            <input className="" type="date" required/>
+                            <input name={"secondaryDate"} onChange={handleChange} value={formData.secondaryDate} id={"secondaryDate"} type="date" required/>
                             <label>Secondary Date</label>
                         </div>
                     </div>
 
                     <div className={"select-holder"}>
-                        <select id={"accommodations"} required>
+                        <select name={"interest"} onChange={handleChange} value={formData.interest} id={"accommodations"} required>
                             <option value=""></option>
                             <option value={"Yes"}>Yes</option>
                             <option value={"No"}>No</option>
@@ -85,23 +143,23 @@ const Contact = () => {
 
                     <div className={"contact-form-two"}>
                         <div className={"field-holder"}>
-                            <input id={"budget"} type={"number"} required/>
+                            <input name={"budget"} onChange={handleChange} value={formData.budget} id={"budget"} type={"number"} required/>
                             <label htmlFor={"budget"}>Event Budget</label>
                         </div>
 
                         <div className={"field-holder"}>
-                            <input id={"guests"} type="number" required/>
+                            <input name={"guests"} onChange={handleChange} value={formData.guests} id={"guests"} type="number" required/>
                             <label htmlFor={"guests"}>Number of Guests</label>
                         </div>
                     </div>
 
                     <div className={"field-holder"}>
-                        <input id={"referral"} type={"text"} required/>
+                        <input name={"hear"} onChange={handleChange} value={formData.hear} id={"referral"} type={"text"} required/>
                         <label htmlFor={"referral"}>How Did You Hear About Us?</label>
                     </div>
 
                     <div className={"field-holder"}>
-                        <input id={"eventdetails"} type={"text"} required/>
+                        <input name={"details"} onChange={handleChange} value={formData.details} id={"eventdetails"} type={"text"} required/>
                         <label htmlFor={"eventdetails"}>Tell Us More About Your Event</label>
                     </div>
                     <button className="submit" type={"submit"}>Submit</button>
