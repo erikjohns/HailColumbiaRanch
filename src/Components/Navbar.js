d123aca19bdbd9e044ac749d66f6016f27430cfd
Johns, Erik Michael <erikj3520@gmail.com>
2024-04-10 14:23:47 -0600

    Temprorary mobile navbar styling

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,5 +1,6 @@
 import './Navbar.css';
 import MenuIcon from '@mui/icons-material/Menu';
+import CloseIcon from '@mui/icons-material/Close';
 // import CloseIcon from '@mui/icons-material/Close';
 import { useState } from 'react';
 
@@ -16,18 +17,23 @@ export default function Navbar () {
             <nav className="main-navbar-mobile">
                 <div className="main-navbar-container-mobile">
                     <a href="/" className="main-navbar-logo-mobile">HCR</a>
-                    <MenuIcon fontSize="large" className="main-navbar-menu-icon" onClick={toggleMenu}/>
+                    {!showMenu && (
+                        <MenuIcon fontSize="large" className="main-navbar-menu-icon" onClick={toggleMenu}/>
+                    )}
                     {showMenu && (
-                        <div className="main-navbar-links-container-mobile">
-                            <a href="/"><span className="main-navbar-hover-underline">Home</span></a>
-                            <a href="/venue"><span className="main-navbar-hover-underline">Venue</span></a>
-                            <a href="/lodge"><span className="main-navbar-hover-underline">Lodge</span></a>
-                            <a href="/gallery"><span className="main-navbar-hover-underline">Gallery</span></a>
-                            <a href="/rentals"><span className="main-navbar-hover-underline">Rentals</span></a>
-                            <a href="/contact"><span className="main-navbar-hover-underline">Contact</span></a>
-                        </div>
+                        <CloseIcon fontSize="large" className="main-navbar-menu-icon" onClick={toggleMenu}/>
                     )}
                 </div>
+                {showMenu && (
+                    <div className="main-navbar-links-container-mobile">
+                        <a href="/"><span className="main-navbar-hover-underline">Home</span></a>
+                        <a href="/venue"><span className="main-navbar-hover-underline">Venue</span></a>
+                        <a href="/lodge"><span className="main-navbar-hover-underline">Lodge</span></a>
+                        <a href="/gallery"><span className="main-navbar-hover-underline">Gallery</span></a>
+                        <a href="/rentals"><span className="main-navbar-hover-underline">Rentals</span></a>
+                        <a href="/contact"><span className="main-navbar-hover-underline">Contact</span></a>
+                    </div>
+                )}
             </nav>
         );
     }
